99907c37e089d113f00a2b1d213616da4b043567
Apps at Techno Study <apps@techno.study>
2020-02-08 07:11:17 -0500

    removed

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,8 +1,6 @@
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -13,14 +11,12 @@ import java.net.URL;
 
 public class SimpleTest {
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @Parameters({"browser"})
     @BeforeClass
     public void setup(String browser) throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setBrowserName(browser);
-        cap.setPlatform(Platform.MAC);
         URL url = new URL("http://localhost:4444/wd/hub");
 
         driver = new RemoteWebDriver(url, cap);
