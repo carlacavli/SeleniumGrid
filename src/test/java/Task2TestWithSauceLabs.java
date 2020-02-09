6215854e07b973971b849b0cbcfd162641c78674
TechnoStudy <info@techno.study>
2020-02-09 16:22:26 -0500

    task 2 solution fix

--- src/test/java/Task2TestWithSauceLabs.java
+++ src/test/java/Task2TestWithSauceLabs.java
@@ -1,15 +1,18 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Task2Test {
-    public static final String KEY = "968e4c07fb278e50883938d14fb859dc";
-    public static final String SECRET = "bf450390f6ef0279e5937a39cdf68be9";
-    public static final String HUB_URL = "http://" + KEY + ":" + SECRET + "@hub.testingbot.com/wd/hub";
+public class Task2TestWithSauceLabs {
+    public static final String KEY = "daulet";
+    public static final String SECRET = "f098ca38-48d4-4f88-9667-acd7336db9c4";
+    public static final String HUB_URL = "https://" + KEY + ":" + SECRET + "@ondemand.saucelabs.com:443/wd/hub";
     private WebDriver driver;
     private URL url;
     private DesiredCapabilities caps;
@@ -28,8 +31,12 @@ public class Task2Test {
         caps.setCapability("browserName", browserName);
         driver = new RemoteWebDriver(url, caps);
         driver.get("https://google.com");
-        driver.quit();
+    }
 
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
     }
 
 
